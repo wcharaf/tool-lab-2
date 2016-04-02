258887d3da208d65bfbb59bbbae7fa3e75155c1c
Aleah Ellis <aleah.is@icloud.com>
2016-04-02 15:35:18 -0400

    it works

--- app.js
+++ app.js
@@ -1,22 +1,31 @@
-var temp=document.getElementById("inputTemp").value;
-var fehr=document.getElementById;("selectF").checked;
-var celc=document.getElementById;("selectC").checked;
 
-function convertor (temp, type) {
+
+function convertor(temp) {
 	// type = type.toLowerCase();
   //if (type==="F")||
-  if (fehr===true) {
-    var ntemp= (temp-32)*(5/9);
-    ntemp.innerHTML = temp+" degrees in F is equal to " +ntemp+" degrees in C";
-    answer.appendChild(ntemp);
+  var temp=document.getElementById("inputTemp").value;
+  var fehr=document.getElementById("selectF").checked;
+  var celc=document.getElementById("selectC").checked;
+
+  var elementTemp=document.createElement('div');
+  console.log(fehr);
+
+
+  if (fehr) {
+    ntemp= (temp-32)*(5/9);
+    elementTemp.innerHTML = temp+" degrees in F is equal to " +ntemp.toFixed(2)+" degrees in C";
+    answer.appendChild(elementTemp);
   }
   //else (type==="C")||
-  else if (celc===true) {
-    ntemp = (temp *(9/5))+32;
-    console.log(temp+" degrees in C is equal to " +ntemp+" degrees in F");
+  else if (celc) {
+    var ntemp = (temp *(9/5))+32;
+    elementTemp.innerHTML= temp+" degrees in C is equal to " +ntemp.toFixed(2)+" degrees in F";
+    answer.appendChild(elementTemp);
   }
   else {
-    console.log("TYPO IDIOT!!!!!")
+    console.log("TYPO IDIOT!!!!!");
   }
-}
-console.log(convertor(100, "c"));
+};
+// console.log(convertor(100, "c"));
+
+// return convertor();
