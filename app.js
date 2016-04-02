f65c74333aed651a9381b49b0022cf61ec6e5726
Jessica Dafforn <jdafforn072@gmail.com>
2016-04-02 14:37:21 -0400

    jess

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-var temp;
-var type;
+var temp=document.getElementById("inputTemp").value;
+var fehr=document.getElementById;("selectF").checked;
+var celc=document.getElementById;("selectC").checked;
+
 function convertor (temp, type) {
 	// type = type.toLowerCase();
   //if (type==="F")||
-  if (type==="f") {
-    ntemp= (temp-32)*(5/9);
-    console.log(temp+" degrees in F is equal to " +ntemp+" degrees in C");
+  if (fehr===true) {
+    var ntemp= (temp-32)*(5/9);
+    ntemp.innerHTML = temp+" degrees in F is equal to " +ntemp+" degrees in C";
+    answer.appendChild(ntemp);
   }
   //else (type==="C")||
-  else if (type==="c") {
+  else if (celc===true) {
     ntemp = (temp *(9/5))+32;
     console.log(temp+" degrees in C is equal to " +ntemp+" degrees in F");
   }
